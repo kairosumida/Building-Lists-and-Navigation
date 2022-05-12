5ed5a4e8c2e038b0113e319002a32953e69de5fd
kairosumida <kairosumida@hotmail.com>
2022-05-12 14:04:48 -0300

    Initial Commit

---
+++ Building Lists and Navigation/Building_Lists_and_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Building_Lists_and_NavigationApp.swift
+//  Building Lists and Navigation
+//
+//  Created by Kairo Sumida on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Building_Lists_and_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
